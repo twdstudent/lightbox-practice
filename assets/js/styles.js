7718be30b4e7481ee06fabea8d3ea7f545fa4dc7
Thomas Dodson <tw.dodson@yahoo.co.uk>
2020-05-02 21:06:16 +0000

    fixed bug in js link

---
+++ assets/js/styles.js
@@ -0,0 +1,10 @@
+const lightbox = document.createElement('div')
+lightbox.id = 'lightbox'
+document.body.appendChild(lightbox)
+
+const images = document.querySelectorAll('img')
+images.forEach(image => {
+    image.addEventListener('click', e => {
+        lightbox.classList.add('active')
+    })
+})
