e904f9f7deb7ff44c5cef530c6258999faa6addb
Thomas Dodson <tw.dodson@yahoo.co.uk>
2020-05-02 21:32:46 +0000

    added js into main html as file wouldn't load

--- assets/js/styles.js
+++ assets/js/styles.js
@@ -6,5 +6,16 @@ const images = document.querySelectorAll('img')
 images.forEach(image => {
     image.addEventListener('click', e => {
         lightbox.classList.add('active')
+        const img = document.createElement('img')
+        img.src = image.src
+        while (lightbox.firstChild) {
+            lightbox.removeChild(lightbox.firstChild)
+        }
+        lightbox.appendChild(img)
     })
+})
+
+lightbox.addEventListener('click', e => {
+    if (e.target !==e.currentTarget) return
+    lightbox.classList.remove('active')
 })
